5faab9af12db6510bb5c3f02afdaa99978cc8a0f
androCyber <tatlicak@gmail.com>
2023-02-16 16:15:51 +0300

    Encode URL parameters

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,6 +1,12 @@
+<?php require_once('../../../private/initialize.php'); ?>
 <?php
   //$id=isset($_GET["id"])? $_GET["id"] : "Parameter not set yet";
   $id= $_GET["id"]?? 0; // PHP > 7.0
 
  echo $id;
 ?>
+<a href="show.php?name=<?php echo u('John Doe') ?>">Link-1</a>
+
+<a href="show.php?company=<?php echo u('Widget&More')?>">Link-2</a>
+
+<a href="show.php?query=<?php echo u('!#*?')?>">Link-3</a>
